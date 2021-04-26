3c6dd9316b68d4ffabf183b7a821a86c75931667
PabloSzx <pablosaez1995@gmail.com>
2021-04-25 22:57:06 -0400

    examples env ports

--- examples/app/express/src/index.ts
+++ examples/app/express/src/index.ts
@@ -14,7 +14,8 @@ buildApp({
 }).then(EnvelopApp => {
   app.use(EnvelopApp);
 
-  app.listen(3000, () => {
-    console.log('Listening on port 3000!');
+  const port = process.env.PORT || 3000;
+  app.listen(port, () => {
+    console.log(`Listening on port ${port}!`);
   });
 });
