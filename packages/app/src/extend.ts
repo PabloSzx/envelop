d8d5eb7799c40912ab5d48e15b8eabd9d214bd6b
PabloSzx <pablosaez1995@gmail.com>
2021-04-26 15:53:15 -0400

    re-export everything @envelop/app/extend module

---
+++ packages/app/src/extend.ts
@@ -0,0 +1,23 @@
+/**
+ * @envelop/app/extend
+ *
+ * Module intended to be used as a building block
+ * for custom Envelop integrations
+ */
+
+export * from './common/app.js';
+export * from './common/base.js';
+export * from './common/dataloader.js';
+export * from './common/scalars.js';
+export * from './common/types.js';
+export * from './common/utils/object.js';
+export * from './common/utils/url.js';
+export * from './common/subscriptions/websocket.js';
+export * from './common/LazyPromise/lazyPromise.js';
+export * from './common/ide/handle.js';
+export * from './common/ide/rawAltair.js';
+export * from './common/codegen/handle.js';
+export * from './common/codegen/outputSchema';
+export * from './common/codegen/prettier.js';
+export * from './common/codegen/typescript.js';
+export * from './common/codegen/write.js';
